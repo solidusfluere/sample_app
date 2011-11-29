8f06de2f0ed6d9450c3ad89c24de3f5dcfd363ba
James Jungsoo Hyun <solidusfluere@gmail.com>
2011-11-30 00:59:26 +0900

    Done with sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,6 +39,11 @@ class User < ActiveRecord::Base
     # return nil if user does not have password.
   end
 
+  def self.authenticate_with_salt(id, cookie_salt)
+    user = find_by_id(id)
+    (user && user.salt == cookie_salt) ? user : nil
+  end
+
   private
 
     def encrypt_password
