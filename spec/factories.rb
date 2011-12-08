7ea217ceafa3ac94a01d5e457abff29d8c8b9267
James Jungsoo Hyun <solidusfluere@gmail.com>
2011-12-08 17:13:08 +0900

    Finish user edit, update, index and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,4 +4,13 @@ Factory.define :user do |user|
   user.email                 "mhartl@example.com"
   user.password              "foobar"
   user.password_confirmation "foobar"
+  user.admin                 false
+end
+
+Factory.sequence :name do |n|
+  "Person #{n}"
+end
+
+Factory.sequence :email do |n|
+  "person-#{n}@example.com"
 end
